43ffa92cf85b684212fc3cbca76c469d6ce45470
Adarsh Panchal <adarshpanchal047@gmail.com>
2026-04-17 00:13:00 +0530

    Library cards, Responsive design commited

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const topselect=document.querySelectorAll(".topselectlist")
+const topselect = document.querySelectorAll(".topselectlist")
 topselect.forEach(e => {
-    e.addEventListener("click",()=>{
-        topselect.forEach(li=>li.classList.remove("active"))
+    e.addEventListener("click", () => {
+        topselect.forEach(li => li.classList.remove("active"))
         e.classList.add("active")
     })
 });
 
-const buttons=document.querySelectorAll(".librarylist")
+const buttons = document.querySelectorAll(".librarylist")
 buttons.forEach(e => {
-    e.addEventListener("click",()=>{
-        buttons.forEach(li=>li.classList.remove("active"))
+    e.addEventListener("click", () => {
+        buttons.forEach(li => li.classList.remove("active"))
         e.classList.add("active")
     })
 });
+
+
